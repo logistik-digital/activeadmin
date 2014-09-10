32f7a44a7675d24b963d3da3eb079d3875d5e4ab
Jack Turnbull <jack.turnbull@logistikgroup.com>
2014-09-10 12:03:39 +0100

    Initial commit: README, changes to devise ConfirmationsController and adding in show view

--- lib/active_admin/devise.rb
+++ lib/active_admin/devise.rb
@@ -75,7 +75,75 @@ module ActiveAdmin
     end
 
     class ConfirmationsController < ::Devise::ConfirmationsController
-       include ::ActiveAdmin::Devise::Controller
+      include ::ActiveAdmin::Devise::Controller
+      # Remove the first skip_before_filter (:require_no_authentication) if you
+      # don't want to enable logged users to access the confirmation page.
+      skip_before_filter :require_no_authentication
+      skip_before_filter :authenticate_user!
+
+      # PUT /resource/confirmation
+      def update
+        with_unconfirmed_confirmable do
+          if @confirmable.has_no_password?
+            @confirmable.attempt_set_password(params[@admin_user_model.name.underscore.to_sym])
+            if @confirmable.valid? and @confirmable.password_match?
+              do_confirm
+            else
+              do_show
+              @confirmable.errors.clear #so that we wont render :new
+            end
+          else
+            @confirmable.errors.add(:email, :password_already_set)
+          end
+        end
+
+        if !@confirmable.errors.empty?
+          self.resource = @confirmable
+          render 'active_admin/devise/confirmations/new'
+        end
+      end
+
+      # GET /resource/confirmation?confirmation_token=abcdef
+      def show
+        with_unconfirmed_confirmable do
+          if @confirmable.has_no_password?
+            do_show
+          else
+            do_confirm
+          end
+        end
+        if !@confirmable.errors.empty?
+          self.resource = @confirmable
+          render 'active_admin/devise/confirmations/new'
+        end
+      end
+
+      protected
+
+      def with_unconfirmed_confirmable
+        # Retrieve admin user model from ActiveAdmin config, in case it isn't AdminUser
+        admin_current_user_method = ActiveAdmin.application.current_user_method
+        @admin_user_model = Object.const_get(admin_current_user_method.to_s.sub("current_","").classify)
+        original_token = params[:confirmation_token]
+        confirmation_token = ::Devise.token_generator.digest(@admin_user_model, :confirmation_token, original_token)
+        @confirmable = @admin_user_model.find_or_initialize_with_error_by(:confirmation_token, confirmation_token)
+        if !@confirmable.new_record?
+          @confirmable.only_if_unconfirmed {yield}
+        end
+      end
+
+      def do_show
+        @confirmation_token = params[:confirmation_token]
+        @requires_password = true
+        self.resource = @confirmable
+        render 'active_admin/devise/confirmations/show'
+      end
+
+      def do_confirm
+        @confirmable.confirm!
+        set_flash_message :notice, :confirmed
+        sign_in_and_redirect(resource_name, @confirmable)
+      end
     end
 
     def self.controllers_for_filters
